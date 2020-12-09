678bb83bc66d6b0a759071eb58eb8a689c26ff97
Matthew Marsandi <matthewmarsandi@gmail.com>
2020-12-09 15:26:28 -0500

    changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -43,7 +43,7 @@ const HomeScreen = () => {
     }
 
     fetchData()
-  }, [date, isValidForNext])
+  }, [date, isValidForNext, dateVal])
 
   const nextHandler = (type) => {
     if (type === 'prev') {
