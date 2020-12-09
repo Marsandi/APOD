91a1713e12da0ea6e2859c31e77a738ee1563c98
Matthew Marsandi <matthewmarsandi@gmail.com>
2020-12-09 15:23:55 -0500

    adding momentjs, added simulated routing with hooks

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Loader from '../components/Loader'
+import moment from 'moment'
 import { API_KEY } from '../api/api'
 
 const HomeScreen = () => {
+  const dateVal = moment().format('YYYY-MM-DD')
   const [mediaUrl, setMediaUrl] = useState('https://via.placeholder.com/500')
   const [loading, setLoading] = useState(true)
   const [mediaType, setMediaType] = useState('image')
   const [title, setTitle] = useState('')
   const [desc, setDesc] = useState('')
-  const [date, setDate] = useState('')
+  const [date, setDate] = useState(dateVal)
+  const [isValidForNext, setIsValidForNext] = useState(true)
   const [searchQuery, setSearchQuery] = useState(
     'https://www.google.com/search?q=NASA'
   )
@@ -17,9 +20,8 @@ const HomeScreen = () => {
   useEffect(() => {
     const fetchData = async () => {
       const mediaObj = await axios.get(
-        `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`
+        `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`
       )
-      console.log(mediaObj)
       if (mediaObj.data.media_type === 'image') {
         setMediaType('image')
       } else if (mediaObj.data.media_type === 'video') {
@@ -28,16 +30,29 @@ const HomeScreen = () => {
       setMediaUrl(mediaObj.data.url)
       setTitle(mediaObj.data.title)
       setDesc(mediaObj.data.explanation)
-      setDate(mediaObj.data.date)
       const tempQuery =
         'https://www.google.com/search?q=' +
         mediaObj.data.title.replace(/ /g, '+')
       setSearchQuery(tempQuery)
       setLoading(false)
+
+      const dayAfter = moment(date).add(1, 'days').format('YYYY-MM-DD')
+      if (moment(dayAfter).isAfter(dateVal)) {
+        setIsValidForNext(false)
+      }
     }
 
     fetchData()
-  }, [])
+  }, [date, isValidForNext])
+
+  const nextHandler = (type) => {
+    if (type === 'prev') {
+      setDate(moment(date).subtract(1, 'days').format('YYYY-MM-DD'))
+      setIsValidForNext(true)
+    } else {
+      setDate(moment(date).add(1, 'days').format('YYYY-MM-DD'))
+    }
+  }
 
   return (
     <>
@@ -89,8 +104,10 @@ const HomeScreen = () => {
           </div>
           <div className='grid grid-cols-2 lg:px-30 xl:px-60 font-body'>
             <div className='flex justify-start lg:pl-5'>
-              <a
-                href={searchQuery}
+              <button
+                onClick={() => {
+                  nextHandler('prev')
+                }}
                 className='rounded py-2 px-8 m-4 text-md text-primary bg-blue-800 hover:bg-white border-2 hover:border-blue-800 text-white hover:text-blue-800 transition ease-out duration-500'
               >
                 <svg
@@ -101,36 +118,40 @@ const HomeScreen = () => {
                   xmlns='http://www.w3.org/2000/svg'
                 >
                   <path
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    stroke-width='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
                     d='M10 19l-7-7m0 0l7-7m-7 7h18'
                   ></path>
                 </svg>
                 <span className='text-base'>Previous Day</span>
-              </a>
+              </button>
             </div>
             <div className='flex justify-end lg:pr-10'>
-              <a
-                href={searchQuery}
-                className='rounded py-2 px-8 m-4 text-md text-primary bg-blue-800 hover:bg-white border-2 hover:border-blue-800 text-white hover:text-blue-800 transition ease-out duration-500'
-              >
-                <span className='text-base'>Next Day</span>
-                <svg
-                  className='inline-block w-6 h-6 ml-3'
-                  fill='none'
-                  stroke='currentColor'
-                  viewBox='0 0 24 24'
-                  xmlns='http://www.w3.org/2000/svg'
+              {isValidForNext && (
+                <button
+                  onClick={() => {
+                    nextHandler('next')
+                  }}
+                  className='rounded py-2 px-8 m-4 text-md text-primary bg-blue-800 hover:bg-white border-2 hover:border-blue-800 text-white hover:text-blue-800 transition ease-out duration-500'
                 >
-                  <path
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    stroke-width='2'
-                    d='M14 5l7 7m0 0l-7 7m7-7H3'
-                  ></path>
-                </svg>
-              </a>
+                  <span className='text-base'>Next Day</span>
+                  <svg
+                    className='inline-block w-6 h-6 ml-3'
+                    fill='none'
+                    stroke='currentColor'
+                    viewBox='0 0 24 24'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                      d='M14 5l7 7m0 0l-7 7m7-7H3'
+                    ></path>
+                  </svg>
+                </button>
+              )}
             </div>
           </div>
         </>
